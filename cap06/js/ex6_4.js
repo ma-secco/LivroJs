f9286cd3b3f3241edaeefdd011ca179d9c4039f8
Maria Secco <mapradosecco@gmail.com>
2023-05-26 12:31:59 -0300

    modified

--- cap06/js/ex6_4.js
+++ cap06/js/ex6_4.js
@@ -14,3 +14,15 @@ frm.addEventListener("submit", (e)=> {
     frm.btListar.dispatchEvent(new Event("click"))
 })
 
+frm.btListar.addEventListener("click", () =>{
+    if(criancas.length == 0) {
+        alert("Não há crianças na lista")
+        return
+    }
+    let lista = "";
+    for(crianca of criancas) {
+        const {nome,idade} = crianca
+        lista += nome + "-" + idade + "anos\n"
+    }
+    resp.innerText = lista;
+})
