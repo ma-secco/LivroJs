773ab90ed2edf308e48a4c2e8f55a087be86a156
Maria Secco <mapradosecco@gmail.com>
2023-06-28 11:17:48 -0300

    new exercise

--- cap06/nodejs/ex6_5.js
+++ cap06/nodejs/ex6_5.js
@@ -3,14 +3,27 @@ const prompt = require("prompt-sync")()
 console.log("Informe os alunos. Após, digite 'Fim' no nome para sair")
 const alunos = [];
 
-
 do {
-    const nome = prompt("Nome aluno: ")
+    const nome = prompt("Nome aluno (a): ")
     if (nome == "Fim" || nome == "fim")  break
 
-    const nota = prompt("Nota: ")
+    const nota = prompt("Nota (0-10): ")
 
     alunos.push({nome,nota})
     console.log(`Ok! ${nome} cadastrado (a)...`)
 }
 while (true)
+
+console.log("-".repeat(40));
+
+const maiorNota = alunos.reduce((a, b) => Math.max(a, b.nota), 0)
+console.log(`Maior nota: ${maiorNota}`)
+if (maiorNota >= 7) {
+    const destaques = alunos.filter(aluno => aluno.nota == maiorNota)
+    for (const destaque of destaques){
+        console.log(`- ${destaque.nome}`)
+    }
+}
+else {
+    console.log('Não há alunos em destaque na turma')
+}
