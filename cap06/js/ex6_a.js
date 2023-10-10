a6c8206a3dd4c1281ab8bd451e6341dd07c24215
Maria Secco <mapradosecco@gmail.com>
2023-10-10 11:32:46 -0300

    created DOM elements

---
+++ cap06/js/ex6_a.js
@@ -0,0 +1,6 @@
+const clube = document.querySelector('#inClube');
+const adicionar = document.querySelector('#btAdicionar');
+const listar = document.querySelector('#btListar');
+const tabela = document.querySelector('#btTabela');
+const resposta = document.querySelector('pre')
+
